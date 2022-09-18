91c707d1a8c314b726e2ff2fffdf0193d9aac07d
bagotaj <bagotaj@gmail.com>
2022-09-18 18:27:44 +0200

    Adding router index.js

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import ToDosView from '@/views/todos/ToDosView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView,
+  },
+  {
+    path: '/todos',
+    name: 'todos',
+    component: ToDosView,
+    props: true,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
