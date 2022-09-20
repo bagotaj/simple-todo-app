ceeb86b40ba9d596bfe7ba7d6b3c10b4f0215130
bagotaj <bagotaj@gmail.com>
2022-09-20 16:24:40 +0200

    Adding display and owner states and their methods which can manipulate the states

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,23 @@ import { createStore } from 'vuex';
 export default createStore({
   state: {
     todos: [],
+    display: false,
+    owner: null,
   },
   getters: {
     doneTodos: (state) => {
       return state.todos.filter((todo) => todo.done);
     },
   },
-  mutations: {},
+  mutations: {
+    setDisplay(state) {
+      state.display = !state.display;
+    },
+    setOwner(state, playload) {
+      state.owner = playload.user;
+      console.log(state.owner, playload.user);
+    },
+  },
   actions: {},
   modules: {},
 });
