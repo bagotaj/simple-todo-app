f8f68ecc37556d7e918d27c4bfb4e88555a91904
bagotaj <bagotaj@gmail.com>
2022-09-20 16:22:17 +0200

    Adding Login component and authRequired to the components which needed

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import ToDosView from '@/views/todos/ToDosView.vue';
 
 const routes = [
   {
     path: '/',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/home',
     name: 'home',
     component: HomeView,
+    meta: {
+      authRequired: true,
+    },
   },
   {
     path: '/about',
     name: 'about',
     component: AboutView,
+    meta: {
+      authRequired: true,
+    },
   },
   {
     path: '/todos',
     name: 'todos',
     component: ToDosView,
     props: true,
+    meta: {
+      authRequired: true,
+    },
   },
 ];
 
