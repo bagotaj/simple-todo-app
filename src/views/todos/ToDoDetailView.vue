fdad84f654954d5beedd729a00195a23b3bb12f5
bagotaj <bagotaj@gmail.com>
2022-09-25 12:29:18 +0200

    Adding url matcher computed property, function and white-space style to p tag

--- src/views/todos/ToDoDetailView.vue
+++ src/views/todos/ToDoDetailView.vue
@@ -1,8 +1,8 @@
 <template>
   <p class="right fontsize12">{{ todo.owner }}</p>
-  <p>{{ todo.todo }}</p>
+  <p v-html="urlMatcherInTodo"></p>
   <a :href="todo.link" target="_blank">{{ todo.link }}</a>
-  <button @click="$store.commit('stopTimer', todo.id)" :class="{ stoped: $store.state.stoped }">Leállít</button>
+  <button @click="$store.commit('stopTimer', todo.id)" :class="{ stopped: $store.state.stopped[todo.id] }">Leállít</button>
   <button class="done" @click="$store.dispatch('updateTodoField', {todoId: todo.id, done: { done: true }})">Kész</button>
 </template>
 
@@ -12,11 +12,20 @@ export default {
     mounted() {
         this.$store.dispatch('setTimeout', {todo: this.todo.todo, todoId: this.todo.id});
     },
+    computed: {
+        urlMatcherInTodo() {
+            const URLMatcher = new RegExp('(?:(?:https?|ftp|file)://|www.|ftp.)(?:([-A-Z0-9+&@#/%=~_|$?!:,.]*)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:([-A-Z0-9+&@#/%=~_|$?!:,.]*)|[A-Z0-9+&@#/%=~_|$])', 'igm')
+            return this.todo.todo.replace(URLMatcher, match => `<a href="${match}" target="_blank">${match}</a>`)
+        }
+    }
 }
 </script>
 
 <style>
-    .stoped {
+    p {
+        white-space: pre-wrap;
+    }
+    .stopped {
         pointer-events: none;
         opacity: 0.3;
     }
