4a8e19a9246ccbd0f6652a9823b3e4f7ec3226de
bagotaj <bagotaj@gmail.com>
2022-09-26 17:14:13 +0200

    Adding aout text and style

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="about">
-      <h1>This is an about page</h1>
+      <h1>Használat</h1>
+      <p>A cél, egy nagyon egyszerűen használható, de kicsit "idegesítő" visszajelzést küldő "todo" applikáció létrehozása volt.</p>
+      <p>A TEENDŐK beviteli mezője akár több sor beírására is alkalmas. Mérete változtatható a jobb alsó sarokban lévő kis háromszöggel.</p>
+      <p>A LINK mezőben külön megadható az url, viszont a TEENDŐK mezőbe írt linkek is automatiusan kattinthatóak lesznek.</p>
+      <p>A "Leállít" gombbal a felugró ablak szüntethető meg.</p>
+      <p>A "Kész" gombbal "elvégzettnek" jelölhető egy teendő és megnyomásával kikerül a listából.</p>
     </div>
 </template>
+
+<style>
+  .about {
+    max-width: 420px;
+    margin: 30px auto;
+  }
+</style>
   
