70b3d7c7e126d1a3978c1be8ca6afe7af78cd777
bagotaj <bagotaj@gmail.com>
2022-10-02 20:47:01 +0200

    Adding scss

--- src/App.vue
+++ src/App.vue
@@ -27,40 +27,41 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-body {
-  margin: 0;
-}
-nav {
-  padding: 60px 20px 40px 20px;
-  font-size: 0.8em;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-weight: bold;
-  background: #f4f4f4;
-}
+  @import 'assets/scss/modules/_icons.scss';
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  body {
+    margin: 0;
+  }
+  nav {
+    padding: 60px 20px 40px 20px;
+    font-size: 0.8em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+    background: #f4f4f4;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #454545;
-  text-decoration: none;
-}
+  nav a {
+    font-weight: bold;
+    color: #454545;
+    text-decoration: none;
+  }
 
-nav a:hover {
-  font-weight: normal;
-}
+  nav a:hover {
+    font-weight: normal;
+  }
 
-nav a.router-link-exact-active {
-  color: #FF3E24;
-}
+  nav a.router-link-exact-active {
+    color: #FF3E24;
+  }
 
-.normalbtn {
-  margin-right: 10px;
-}
+  .normalbtn {
+    margin-right: 10px;
+  }
 </style>
