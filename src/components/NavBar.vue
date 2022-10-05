9401fd8aa0e61c31a87393cdf0eaa32b01189281
bagotaj <bagotaj@gmail.com>
2022-10-05 11:16:20 +0200

    Moving navbar elements and router-link into a separate div. Adding new navMenu style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,20 @@
 <template>
   <nav v-if="$store.state.display">
-    <router-link to="/home">Home</router-link> |
-    <router-link :to="{ name: 'about'}">Az appról</router-link> |
-    <router-link :to="{ name: 'todos'}">Teendők</router-link> |
-    <button
-      type="button"
-      class="normalbtn"
-      value="logout"
-      @click="logout"
-      >
-      Kilépés
+    <div class="navMenu">
+      <div>
+        <router-link class="marginRight10" to="/home">Home</router-link>
+        <router-link class="marginRight10" :to="{ name: 'about'}">Az appról</router-link>
+        <router-link :to="{ name: 'todos'}">Teendők</router-link>
+      </div>
+      <button
+        type="button"
+        class=""
+        value="logout"
+        @click="logout"
+        >
+        Kilépés
       </button>
+    </div>
   </nav>
 </template>
 
@@ -35,3 +39,13 @@ export default {
   }
 }
 </script>
+
+<style>
+  .navMenu {
+    min-width: 420px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
