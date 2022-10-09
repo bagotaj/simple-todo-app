95366bdcb94cbe27e8198fcfc055bb856f67eb92
József Bagota <jozsef.bagota@gmail.com>
2022-10-09 21:20:51 +0200

    Creating login test with username and password

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,19 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('http://localhost:8080');
-    cy.contains('h1', 'Belépés');
+import { describe } from 'mocha';
+
+// describe('My First Test', () => {
+//   it('Visits the app root url', () => {
+//     cy.visit('http://localhost:8080');
+//     cy.contains('h1', 'Belépés');
+//   });
+// });
+
+describe('Login test', () => {
+  it.only('Check login with correct password', () => {
+    cy.visit('/');
+    cy.get(':nth-child(1) > .form-control').type(Cypress.env('user_name'));
+    cy.get(':nth-child(2) > .form-control').type(Cypress.env('user_password '));
+    cy.get('#submitButton').click();
   });
 });
