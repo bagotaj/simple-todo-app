0db326230cfcd8115071134312d6264356590145
József Bagota <jozsef.bagota@gmail.com>
2022-10-11 20:49:50 +0200

    Adding Login, prompt, alert and popup tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,18 +2,51 @@
 
 import { describe } from 'mocha';
 
-// describe('My First Test', () => {
-//   it('Visits the app root url', () => {
-//     cy.visit('http://localhost:8080');
-//     cy.contains('h1', 'Belépés');
+// describe('Login test', () => {
+//   it.only('Check login with correct password', () => {
+//     cy.visit('/');
+//     cy.get(':nth-child(1) > .form-control').type(Cypress.env('user_name'));
+//     cy.get(':nth-child(2) > .form-control').type(Cypress.env('user_password '));
+//     cy.get('#submitButton').click();
 //   });
 // });
 
-describe('Login test', () => {
-  it.only('Check login with correct password', () => {
-    cy.visit('/');
+// describe('Prompt Test', function () {
+//   // test case
+//   it('Scenario 1', function () {
+//     //URL launch
+//     cy.visit('http://localhost:8081/');
+//     //handling prompt alert
+//     cy.window().then((win) => {
+//       //stubbing prompt and alert windows
+//       cy.stub(win, 'prompt').returns('Joseph');
+//       cy.stub(win, 'alert').as('alert');
+//     });
+//     cy.get('#switch-register').click();
+//     cy.get('@alert').should(
+//       'have.been.calledWith',
+//       'Hello Joseph! How are you today?'
+//     );
+//   });
+// });
+
+describe('Pop up Test', () => {
+  it('Login', () => {
+    cy.visit('http://localhost:8081/');
     cy.get(':nth-child(1) > .form-control').type(Cypress.env('user_name'));
-    cy.get(':nth-child(2) > .form-control').type(Cypress.env('user_password '));
+    cy.get(':nth-child(2) > .form-control').type(Cypress.env('user_password'));
     cy.get('#submitButton').click();
+  });
+  it('Check popup', () => {
+    const popup_url = 'http://youtube.com';
+    cy.window().then((win) => {
+      cy.stub(win, 'open').as('windowopen');
+    });
+    cy.get('#popupWindow').click();
+    //cy.get('@windowopen').should('have.been.calledWidth', popup_url);
+    cy.window().then((win) => {
+      win.location.href = popup_url;
+      cy.get('input#search').type('Cypress by qa box lets test{enter}');
+    });
   });
 });
