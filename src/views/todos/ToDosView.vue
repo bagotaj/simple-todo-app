e307c45bf6f8ebd58d3d2241e4c65109b2608ba7
József Bagota <jozsef.bagota@gmail.com>
2023-05-11 18:45:38 +0200

    adding container class and new style to the component

--- src/views/todos/ToDosView.vue
+++ src/views/todos/ToDosView.vue
@@ -1,15 +1,14 @@
 <template>
   <h1>Teendők</h1>
-    <div v-if="$store.state.todos.length">
-      <div v-for="todo in reversedTodos" :key="todo.id">
-        <div v-if="!todo.done" class="todo">
-          <ToDoDetailView :todo="todo" />
-        </div>
+    <div v-if="$store.state.todos.length" class="container">
+      <div v-for="todo in reversedTodos" :key="todo.id" class="todo">
+        <ToDoDetailView :todo="todo" />
       </div>
     </div>
-  </template>
+    <div v-else>Nincs teendőd!</div>
+</template>
   
-  <script>
+<script>
   import ToDoDetailView from './ToDoDetailView.vue';
   
   export default {
@@ -19,16 +18,33 @@
         return [...this.$store.state.todos].reverse()
       }
     }
-}
-  </script>
+  }
+ </script>
   
-  <style>
+<style>
+    .container {
+      max-width: 1380px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+    @media (max-width: 1024px) {
+      .container {
+        max-width: 960px;
+      }
+    }
+    @media (max-width: 768px) {
+      .container {
+        max-width: 480px;
+      }
+    }
     .todo {
       background: #f4f4f4;
       padding: 20px;
       border-radius: 10px;
-      margin: 10px auto;
-      max-width: 420px;
+      margin: 10px;
+      width: 400px;
       color: #454545;
       display: flex;
       flex-direction: column;
@@ -38,4 +54,4 @@
       color:#FF3E24;
       cursor: pointer;
     }
-  </style>
+</style>
